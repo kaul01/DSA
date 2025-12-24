4829bcefe7f3cb6da10f5671aa88202ec12b5dcf
Saiesh Kaul <saieshkaul01@gmail.com>
2025-12-23 20:29:18 -0800

    leetcode 206

---
+++ leetcode/L206.java
@@ -0,0 +1,39 @@
+public class L206 {
+    public static ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode ans = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = ans;
+            ans = head;
+            head = next;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ListNode result = reverseList(head);
+        System.out.println("Reversed list: ");
+        printList(result);
+    }
+
+    public static void printList(ListNode head) {
+        while(head != null){
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
