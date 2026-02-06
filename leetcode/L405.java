9f4fd5321c54a61b8e852f73a270ec2afbc25cb5
Saiesh Kaul <saieshkaul01@gmail.com>
2026-02-05 19:00:18 -0700

    leetcode 405

---
+++ leetcode/L405.java
@@ -0,0 +1,18 @@
+public class L405 {
+    public static String toHex(int num) {
+        if(num == 0) return "0";
+
+        String hex = "0123456789abcdef";
+        StringBuilder sb = new StringBuilder();
+
+        while(num != 0){
+            int digit = num & 15;
+            sb.append(hex.charAt(digit));
+            num >>>= 4;
+        }
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(toHex(26));
+    }
+}
