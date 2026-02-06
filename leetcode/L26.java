4e74c625cbc82e863859da249bf9c7539d1391d1
Saiesh Kaul <saieshkaul01@gmail.com>
2026-02-05 17:10:20 -0700

    leetcode 26

---
+++ leetcode/L26.java
@@ -0,0 +1,15 @@
+public class L26 {
+    public static int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=1 ; j<nums.length ; j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{1,1,2}));
+    }
+}
