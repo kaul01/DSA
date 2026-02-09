c3c20411c61b650d7daabc96c02bbd2ea35a6b7e
Saiesh Kaul <saieshkaul01@gmail.com>
2026-02-08 17:37:24 -0700

    leetcode 144

--- leetcode/L94.java
+++ leetcode/L94.java
@@ -10,12 +10,25 @@ public class L94 {
         inorder(root.left, res);
         res.add(root.val);
         inorder(root.right, res);
-    } 
+    }
+    
+    public static List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        preorder(root, res);
+        return res;
+    }
+    public static void preorder(TreeNode root, List<Integer> res){
+        if(root == null)return;
+        res.add(root.val);
+        preorder(root.left, res);
+        preorder(root.right, res);
+    }
+    
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.right = new TreeNode(2);
         root.right.left = new TreeNode(3);
 
-        System.out.println(inorderTraversal(root));
+        System.out.println(preorderTraversal(root));
     }
 }
