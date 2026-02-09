a0b80a1d3ba31df9a9988ed2ad749bec99216f7e
Saiesh Kaul <saieshkaul01@gmail.com>
2026-02-08 17:56:21 -0700

    leetcode bottomView

--- leetcode/L199.java
+++ leetcode/L199.java
@@ -23,12 +23,26 @@ public class L199 {
         return list;
         
     }
+
+    public static List<Integer> bottomView(TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        if(root == null)return res;
+        dfs(root, res);
+
+        return res;
+    }
+    public static void dfs(TreeNode root, List<Integer> res){
+        if(root == null) return;
+        if(root.left == null && root.right == null) res.add(root.val);
+        dfs(root.left, res);
+        dfs(root.right, res);
+    }
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
         root.left.right = new TreeNode(5);
         root.right.right = new TreeNode(4);
-        System.out.println(rightSideView(root));
+        System.out.println(bottomView(root));
     }
 }
